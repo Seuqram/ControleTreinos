67cd8470b8c4348ead595278d98afe03349429e8
Rodrigo Marques <rodrigo.marques@uniriotec.br>
2016-11-07 18:37:09 -0200

    Herança Aluno Pessoa OK

--- src/modelo/Aluno.java
+++ src/modelo/Aluno.java
@@ -30,54 +30,6 @@ public class Aluno extends Pessoa implements Serializable{
 	public Aluno(){
 		
 	}
-
-/*	public String getNome() {
-		return nome;
-	}*/
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public int getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(int cpf) {
-		this.cpf = cpf;
-	}
-
-	public String getData_nasc() {
-		return data_nasc;
-	}
-
-	public void setData_nasc(String data_nasc) {
-		this.data_nasc = data_nasc;
-	}
-
-	public int getMatricula() {
-		return matricula;
-	}
-
-	public void setMatricula(int matricula) {
-		this.matricula = matricula;
-	}
-
-	public int getNivelacesso() {
-		return nivelacesso;
-	}
-
-	public void setNivelacesso(int nivelacesso) {
-		this.nivelacesso = nivelacesso;
-	}
 	
 	public void incluiAluno(String nome, String email, int cpf, String data_nasc, int matricula, int nivelacesso ){
 		Aluno aluno = new Aluno();
